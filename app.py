59dd9286c074505e59c2da71cb53c98784fdcac8
marlee <marlee@redhat.com>
2022-04-20 20:54:12 -0400

    Added more questions. Currently some of the functions does not perform correctly.

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-# import streamlit as st
-# import pandas as pd
-# import numpy as np
-
 from flask import Flask, render_template, request
 import random, copy
 import questions
@@ -18,67 +14,28 @@ def shuffle(q):
  i = 0
  while i < len(q):
   current_selection = random.choice(list(q.keys()))
+  #current_selection = list(q.keys())
   if current_selection not in selected_keys:
    selected_keys.append(current_selection)
    i = i+1
  return selected_keys
 
+questions_shuffled = shuffle(copy_questions)
+
 @app.route('/')
 def quiz():
- questions_shuffled = shuffle(copy_questions)
+ #questions_shuffled = shuffle(copy_questions)
  for i in copy_questions.keys():
   random.shuffle(copy_questions[i])
  return render_template('main.html', q = questions_shuffled, o = copy_questions)
 
+@app.route('/solution', methods=['POST'])
+def quiz_answers():
+  # for i in copy_questions.keys():
+  #   answered = request.form[i]
+  #   if questions.original_questions[i][0] == answered:
+  return render_template('quiz_solutions.html',q = questions_shuffled, o = copy_questions)
 
 
 if __name__ == '__main__':
  app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# st.title("SRE-U Self Assessment")
-
-# q1choices = ('Foolish or boorish person.','A way to get and publish software packages ','A common unix command used to visualize code.','A source control tool.')
-# list_q1choices = list(q1choices)
-# q1_input = st.radio("1. What is git?",q1choices)
-# # question, answer, solution
-
-# for i in list_q1choices:
-#     if i==list_q1choices[0]:
-#         st.write("You got the right answer")
-#         break
-#     else:
-#         st.write("wrong")
-#         break
-# # Need to have options for unseelct
-
-
-
-# hide_st_style = """
-#             <style>
-#             footer {visibility: hidden;}
-#             </style>
-#             """
-# st.markdown(hide_st_style, unsafe_allow_html=True)
-
