0d1c580fad8b82fc7bcf55afb7e250165766fd0a
marlee <marlee@redhat.com>
2022-04-21 21:32:26 -0400

    Was able to get some of the questions to work

--- main.py
+++ main.py
@@ -1,43 +1,63 @@
 from faulthandler import disable
 import streamlit as st
 import pandas as pd
+import questions
 
 st.title("Red Hat SRE-U Self Assessment")
 
-# genre = st.radio("Genre",('Comedy','Drama'),index=0)
-# correct_genre = 'Comedy'
+question_list = questions.questions_dic
 
-q1 = st.write("What is git?")
-response1 = 'Foolish or boorish person.'
-response2 = 'A way to get and publish software packages '
-response3 = 'A common unix command used to visualize code.'
-response4 = 'A source control tool.'
-
-option1 = st.checkbox(response1)
-option2 = st.checkbox(response2)
-option3 = st.checkbox(response3)
-option4 = st.checkbox(response4)
-
-
-# if genre != "":
-#   st.write(f'You selected {genre}')
-# else:
-#   st.write("No selection")
-
-submit_button = st.button("Submit")
-
-if submit_button:
-  if option1 and not (option2 or option3 or option4):
-    st.write(f'Correct Answer: {response1}')
-  else:
-    st.write(f'Correct Answer: {response1}')
-  #if genre != "":
-  # if genre:
-  #   st.write(f'You selected {genre}')
-  #   if genre==correct_genre:
-  #     st.write(f'Correct Answer: {correct_genre}')
-  #   else:
-  #     st.write("Incorrect selection")
+question_count = 1
+for key,value in question_list.items():
+  string_number = str(question_count)
+  for inner_key, inner_value in value.items():
+      if inner_key == 'question':
+          question_output = st.write(f'{string_number}. {inner_value}')
+      elif inner_key == 'option1':
+          option1 = st.checkbox(inner_value)
+      elif inner_key == 'option2':
+          option2 = st.checkbox(inner_value)
+      elif inner_key == 'option3':
+          option3 = st.checkbox(inner_value)
+      elif inner_key == 'option4':
+          option4 = st.checkbox(inner_value)
+      elif inner_key == 'option5':
+          option5 = st.checkbox(inner_value)
+    #break
+  submit_button = st.button("Submit")
+  question_count += 1
 
 
-#if __name__ == '__main__':
+# for key, value in question_list.items():
+
+  #question = st.write()
+
+# q1 = st.write("What is git?")
+# response1 = 'Foolish or boorish person.'
+# response2 = 'A way to get and publish software packages '
+# response3 = 'A common unix command used to visualize code.'
+# response4 = 'A source control tool.'
+
+# option1 = st.checkbox(response1)
+# option2 = st.checkbox(response2)
+# option3 = st.checkbox(response3)
+# option4 = st.checkbox(response4)
+
+# submit_button = st.button("Submit")
+
+# # if submit_button:
+# #   q_and_a_count = 1
+# #   for key,value in question_list.items():
+# #     string_number = str(q_and_a_count)
+# #     for inner_key, inner_value in value.items():
+# #       if key == "q1" and inner_key == "answer":
+# #         st.write(f'{string_number}. Correct answer: {inner_value}')
+# #       elif key == "q2" and inner_key == "answer":
+# #         st.write(f'{string_number}. Correct answer: {inner_value}')
+# #     q_and_a_count += 1
+
+# if submit_button:
+#   # if option1 and not (option2 or option3 or option4):
+#   #   st.write(f'Correct Answer: {response1}')
+#   # else:
+#   #   st.write(f'Correct Answer: {response1}')
---
+++ questions.py
@@ -0,0 +1,61 @@
+import streamlit as st
+questions_dic = {
+    'q1': {
+        'question':'What is git?',
+        'option1': 'Foolish or boorish person.',
+        'option2': 'A way to get and publish software packages ',
+        'option3': 'A common unix command used to visualize code.',
+        'option4': 'A source control tool.',
+        'answer': 'Foolish or boorish person.'
+    },
+    'q2': {
+        'question':'What is a public repository?',
+        'option1': 'One in which developers by default have full edit privileges.',
+        'option2': 'A repository globally accessible within the organization.',
+        'option3': 'A repository that has been made publicly viewable, meaning it can be forked and viewed by anyone, but maintainers control the commits.',
+        'option4': 'An open source repository.',
+        'answer': 'A repository that has been made publicly viewable, meaning it can be forked and viewed by anyone, but maintainers control the commits.'
+    },
+}
+
+# for key,value in questions_dic.items():
+#     for inner_key, inner_value in value.items():
+#         if inner_key == 'question':
+#             #question_output = st.write(inner_value)
+#             pass
+#         elif inner_key == 'option1':
+#             st.checkbox(inner_value)
+#         elif inner_key == 'option2':
+#             st.checkbox(inner_value)
+#         elif inner_key == 'option3':
+#             st.checkbox(inner_value)
+#         elif inner_key == 'option4':
+#             st.checkbox(inner_value)
+#         elif inner_key == 'option5':
+#             st.checkbox(inner_value)
+#     break
+
+
+# q1 = st.write("What is git?")
+# response1 = 'Foolish or boorish person.'
+# response2 = 'A way to get and publish software packages '
+# response3 = 'A common unix command used to visualize code.'
+# response4 = 'A source control tool.'
+
+# option1 = st.checkbox(response1)
+# option2 = st.checkbox(response2)
+# option3 = st.checkbox(response3)
+# option4 = st.checkbox(response4)
+
+"""
+{
+  question#:{
+    options: [option1,option2,option3]
+    answer: 
+    },
+  question#:{
+    options: [option1,option2,option3]
+    answer: 
+    },
+}
+"""
