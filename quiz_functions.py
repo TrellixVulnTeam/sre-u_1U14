c8e2cf9b52b8736942ccfe693514347ff135e4ba
marlee <marlee@redhat.com>
2022-05-04 22:45:14 -0400

    Getting rid of unnecssary comments:

--- quiz_functions.py
+++ quiz_functions.py
@@ -33,23 +33,9 @@ def display_solution():
         string_question = f"q"+string_number
         for inner_key, inner_value in value.items():
             if inner_key == 'question':
-                #question_output = st.write(f'**{string_number}. {inner_value}**')
                 st.markdown(f"<p float:left; clear: both; style=color:Red;><strong>{string_number}. {inner_value}</strong></p>", unsafe_allow_html=True)
-                #st.markdown(f'**{string_number}. {inner_value}**')
             if inner_key == "answer":
-            #if key == string_question and inner_key == "answer":
-
-                #st.write(f'{string_number}. Correct answer: {inner_value}')
-                #container = st.container()
-                #container.write(f'{string_number}. {inner_value}')
-                #st.text(f'{string_number}. Correct answer: {inner_value}')
-                #<div style = "text-align: left">
-                #st.markdown(f"<h6 style='text-align: left;'>{string_number}. Correct answer: {inner_value}</h6>",unsafe_allow_html=True)
-                #st.markdown(f"<p float:left; clear: both;><strong>{string_number}.</strong> Correct answer: {inner_value}</p>",unsafe_allow_html=True)
                 st.markdown(f"<p float:left; clear: both;>{inner_value}</p>",unsafe_allow_html=True)
-            #elif key == string_question and string_question == "q3" and inner_key =="Reason":
             elif string_question == "q3" and inner_key =="Reason":
                 st.markdown(f'<p float:left; clear: both;> Reason: {inner_value}<p>',unsafe_allow_html=True)
-                #st.text(f'Reason: {inner_value}')
-            #st.write('\n')
         q_and_a_count += 1
